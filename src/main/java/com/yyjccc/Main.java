f8fcad55c630a87924ec2dace5e4c7b786384fb8
yyj <yanyongjuyyj@outlook.com>
2023-12-25 21:49:31 +0800

    test1

---
+++ src/main/java/com/yyjccc/Main.java
@@ -0,0 +1,13 @@
+package com.yyjccc;
+
+import com.yyjccc.CommonsExp.Cc1;
+import com.yyjccc.Utils.Tool;
+
+// 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
+// 然后按 Enter 键。现在，您可以在代码中看到空格字符。
+public class Main {
+    public static void main(String[] args) throws Exception {
+       // Cc1.cc1_1();
+        Tool.unserialize();
+    }
+}
